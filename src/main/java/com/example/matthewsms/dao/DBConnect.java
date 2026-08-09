f491506952397daec03671c2f6e48db8bd3b787a
fidelis <fidelisnicolas@gmail.com>
2026-08-09 21:05:28 +0100

    Prepared Statement Practice

--- src/main/java/com/example/matthewsms/dao/DBConnect.java
+++ src/main/java/com/example/matthewsms/dao/DBConnect.java
@@ -1,28 +1,32 @@
 package com.example.matthewsms.dao;
 
+import com.example.matthewsms.entity.Subject;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class DBConnect {
     public static void main(String[] args) {
         try {
             Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/matthew_school_management", "root", "");
             System.out.println("Connection successful");
+            
+            Subject subject = new Subject();
+            subject.setSubjectCode("GEO");
+            subject.setSubjectName("Geography");
+            subject.setDescription("Basic Geo");
 
-            String sql = "SELECT * FROM subject";
-            Statement statement = connection.createStatement();
-            ResultSet resultSet = statement.executeQuery(sql);
+            String sql = "INSERT INTO subject VALUES (null, ?, ?, ?, now(), now())";
 
-            while(resultSet.next()) {
-            System.out.println(resultSet.getString("subject_name"));
-            System.out.println(resultSet.getString("subject_code"));
-            System.out.println(resultSet.getString("description"));
+            PreparedStatement ps = connection.prepareStatement(sql);
+            ps.setString(1, subject.getSubjectName());
+            ps.setString(2, subject.getSubjectCode());
+            ps.setString(3, subject.getDescription());
 
-            System.out.println("-----------------------------");
-            }
+            ps.executeUpdate();
+            
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
