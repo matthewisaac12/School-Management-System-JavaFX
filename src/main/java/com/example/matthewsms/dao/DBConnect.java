3e212a480694444817c6b6ebea8544933b703f13
fidelis <fidelisnicolas@gmail.com>
2026-08-09 21:23:59 +0100

    Prepared Statement Practice

--- src/main/java/com/example/matthewsms/dao/DBConnect.java
+++ src/main/java/com/example/matthewsms/dao/DBConnect.java
@@ -9,13 +9,8 @@ import java.sql.SQLException;
 
 public class DBConnect {
 
-    public static Connection connect() {
-        Connection connection;
-        try {
-            return connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/matthew_school_management", "root", "");
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public static Connection connect() throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/matthew_school_management", "root", "");
+        return connection;
     }
 }
